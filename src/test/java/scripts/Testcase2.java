7366adf30cd8cc1e30bbeca1f09e2d74dcc029df
shara <shara@LAPTOP-JCCI2VBB>
2021-12-13 19:09:00 +0530

    jenkin

---
+++ src/test/java/scripts/Testcase2.java
@@ -0,0 +1,12 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Testcase2 {
+@Test
+	public void tc2()
+	{
+		Reporter.log("Hello VB",true);
+	}
+}
---
+++ src/test/java/scripts/Testcases1.java
@@ -0,0 +1,12 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Testcases1 {
+@Test
+	public void tc1()
+	{
+		Reporter.log("Hello java",true);
+	}
+}
